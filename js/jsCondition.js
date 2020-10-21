e002265a327f5ec9bbe7c197b06cb1d88eb583e2
Md.Noushedul Islam <noushedakib@gmail.com>
2020-10-21 13:11:29 +0600

    if,if else,else if

---
+++ js/jsCondition.js
@@ -0,0 +1,43 @@
+// if condition
+var a = 10;
+if(a==10){
+	console.log("Hello web designer"); 
+}
+
+// if else condition
+var b = 10;
+if(a==12){
+	console.log("You are right");
+}else{
+	console.log("I think, you are right.")
+}
+
+// else if
+var c = 12;
+if(c==11){
+	console.log("USA");
+}else if(c==8){
+	console.log("UK");
+}else if(c<2){
+	console.log("Russia");
+}else{
+	console.log("Bangladesh");
+}
+
+// break
+var d = 10;
+for(i=0;i<d;i++){
+	if(i==4){
+		break;
+	}
+	console.log(i);
+}
+
+// continue
+var e =7;
+for(k=0;k<e;k++){
+	if(k==5){
+		continue;
+	}
+	console.log(k);
+}
